0abca98030c27efa0b3f9311522ed3e3c2ef4345
Dmytro0077 <kaktusgren@gmail.com>
2022-08-08 10:35:46 +0300

    Finish homework and homeworkextra

---
+++ Lesson3/Lesson3/Homework.cs
@@ -0,0 +1,35 @@
+﻿namespace Lesson3
+{
+    internal class Homework
+    {
+        static void Homework1()
+        {
+            int x = 5;
+            int y = 2;
+            int sum = 0;
+
+            if (x == y)
+            {
+                Console.WriteLine(x);
+            }
+
+            else if (x < y)
+            {
+                for (int i = x; i <= y; i++)
+                {
+                    sum = sum + i;
+                }
+                Console.WriteLine(sum);
+            }
+
+            else
+            {
+                for (int i = y; i <= x; i++)
+                {
+                    sum = sum + i;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ Lesson3/Lesson3/HomeworkExtra.cs
@@ -0,0 +1,50 @@
+﻿namespace Lesson3
+{
+    internal class HomeworkExtra
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть х:");
+            string? x1 = Console.ReadLine();
+
+            Console.WriteLine("Введіть y:");
+            string? y1 = Console.ReadLine();
+
+            int sum = 0;    
+                        
+            bool resultx = int.TryParse(x1, out int x);
+            bool resulty = int.TryParse(y1, out int y);
+           
+            if (resultx && resulty)
+            {               
+                if (x == y)
+                {
+                    Console.WriteLine($"Cума чисел між числами x та y включно = {x}");
+                }
+
+                else if (x < y)
+                {
+                    for (int i = x; i <= y; i++)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine($"Cума чисел між числами x та y включно = {sum}");
+                }
+
+                else
+                {
+                    for (int i = y; i <= x; i++)
+                    {
+                        sum  += i;
+                    }
+                    Console.WriteLine($"Cума чисел між числами x та y включно = {sum}");
+                }                
+            }
+
+            else
+            {
+                Console.WriteLine("Введено некоректні дані. Будь ласка, введіть коректні числа х та у.");               
+            }            
+        }
+    }
+}
